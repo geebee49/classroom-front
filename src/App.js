652d76689a0b8aa6f963c20b30b0541265052780
Goutham Byju <goutham.byju49@gmail.com>
2021-08-17 12:49:05 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { ReactDOM } from 'react';
+import {BrowserRouter, Route,Link} from 'react-router-dom'
+import login from './components/login';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <BrowserRouter>
+      <div>
+        <Route path='/login' component={login}></Route>
+      </div>
+    </BrowserRouter>
     </div>
   );
 }
