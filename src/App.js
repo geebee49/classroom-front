cbc2234ec50baabf9fc1dcd31f1412f9d3cd0cfa
anikaitsunil <anikait.sunilnair@gmail.com>
2021-08-18 11:51:38 +0530

    New commitment

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import login from './components/login';
 import Student from './components/edit';
 import signup from './components/signup';
 import Welcome from './components/welcome';
-<<<<<<< HEAD
 import MiniDrawer from './components/bleh';
-=======
->>>>>>> 516f62c64b2f550489e2d0d5587f085431b6bdbc
 
 
 function App() {
@@ -21,11 +18,8 @@ function App() {
         <Route path='/edit' component={Student}></Route>
         <Route path='/signup' component={signup}></Route>
         <Route path='/welcome' component={Welcome}></Route>
-<<<<<<< HEAD
         <Route path='/mainpage' component={MiniDrawer}></Route>
         
-=======
->>>>>>> 516f62c64b2f550489e2d0d5587f085431b6bdbc
       </div>
     </BrowserRouter>
     </div>
