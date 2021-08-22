fb61c470908e41a648139927d4690dc3d8d36001
Goutham Byju <64029301+geebee49@users.noreply.github.com>
2021-08-22 13:28:30 +0530

    edit page start

--- src/components/edit.js
+++ src/components/edit.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
-
+import { TextField } from "@material-ui/core";
 import "./styles/edit.css";
+import MiniDrawer from "./student-sidebar";
 
 
 const Student = () => {
@@ -8,24 +9,41 @@ const Student = () => {
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
   return (
-    <div id="editPage">
-      
+    <div className="editPage">
+        <MiniDrawer/>
       <div className="edit">
         <div className="bio">
           <h1>EDIT PROFILE</h1>
+          <div>
           <div className="textfields">
-            {/* <TextField
+             <TextField
               value={name}
               label="Enter your name"
               onChange={(e) => {
                 setName(e.target.value);
               }}
-            /> */}
+            /> 
+            <TextField
+              value={email}
+              type="password"
+              label="New Password"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <TextField
+              value={phone}
+              type="password"
+              label="Confirm Password"
+              onChange={(e) => {
+                setPhone(e.target.value);
+              }}
+            />
+          </div>
+          <img src="https://in.bmscdn.com/iedb/artist/images/website/poster/large/mammootty_1340_29-07-2016_11-43-24.jpg" alt="xyz" width="100px" height="100px"></img>
           </div>
         </div>
-        <div className="profilePicture">
-          <h1>Public/Private</h1>
-        </div>
+      
       </div>
     </div>
   );
