8fa3112325f1a0ae5fb5af05029c624a61c4bbfb
Maria Viji George <mariavijigeorge@gmail.com>
2021-08-26 16:18:13 +0530

    teacher-2 branch third commit

--- src/components/buttonGroup.jsx
+++ src/components/buttonGroup.jsx
@@ -7,12 +7,48 @@ import VideocamIcon from '@material-ui/icons/Videocam';
 import VideocamOffIcon from '@material-ui/icons/VideocamOff';
 import Styles from "./styles/buttongroup.css";
 
-export default function(){
+export default function ButtonGroup(){
+    const[voice,setVoice]= React.useState(true);
+    const[voiceIcon,setVoiceIcon]= React.useState(<MicIcon />);
+    const handleVoice = function() {
+        if(voice==true){
+            setVoiceIcon(<MicOffIcon />);
+            setVoice(false);
+        }
+        else{
+            setVoiceIcon(<MicIcon />);
+            setVoice(true);
+        }
+    };
+    const[video,setVideo]= React.useState(true);
+    const[videoIcon,setVideoIcon]= React.useState(<VideocamIcon />);
+    const handleVideo = () => {
+        if(video==true){
+            setVideoIcon(<VideocamOffIcon />);
+            setVideo(false);
+        }
+        else{
+            setVideoIcon(<VideocamIcon />);
+            setVideo(true);
+        }
+    };
+    const[screen,setScreen]= React.useState(false);
+    const[screenIcon,setScreenIcon]= React.useState(<StopScreenShareIcon />);
+    const handleScreen = () => {
+        if(screen==true){
+            setScreenIcon(<StopScreenShareIcon />);
+            setScreen(false);
+        }
+        else{
+            setScreenIcon(<ScreenShareIcon />);
+            setScreen(true);
+        }
+    };
     return(
         <div>
-            <button className="screenshare"><ScreenShareIcon /></button>
-            <button className="voice"><MicOffIcon /></button>
-            <button className="video"><VideocamIcon /></button>
+            <button className="screenshare" onClick={handleScreen}>{screenIcon}</button>
+            <button className="voice" onClick={handleVoice}>{voiceIcon}</button>
+            <button className="video" onClick={handleVideo}>{videoIcon}</button>
         </div>
     );
 }
