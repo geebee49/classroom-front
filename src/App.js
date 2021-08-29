ab768fd3527a5371b68b9dd12cd9b99abc62a6b2
anikaitsunil <anikait.sunilnair@gmail.com>
2021-08-29 20:32:33 +0530

    join page added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Signup from './components/signup';
 import Welcome from './components/welcome';
 import Studentpage from './components/student-mainpage';
 import Login_teacher from './components/login-teacher';
+import Join from './components/join';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <div>
         <Route path='/login' component={Login}></Route>
         <Route path='/edit' component={Student}></Route>
+        <Route path='/join' component={Join}></Route>
         <Route path='/signup' component={Signup}></Route>
         <Route path='/welcome' component={Welcome}></Route>
         <Route path='/mainpage' component={Studentpage}></Route>
---
+++ src/components/join.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { TextField } from "@material-ui/core";
+import "./styles/join.css";
+import MiniDrawer from "./student-sidebar";
+
+
+const Join = () => {
+  const [name, setName] = useState("");
+  return (
+    <div className="joinPage">
+        <MiniDrawer/>
+        <div className="join">
+            <div className="headerJoin">
+                <img src="add.png" alt="bleh" width="40px" height="40px"/>
+                <h1>JOIN A CLASSROOM</h1>
+            </div>
+            <div className="code">
+                <h3>Enter Your Classroom Code</h3>
+                <div className="textfieldsJoin">
+                    <TextField id="outlined-basic" label="Enter your Name" variant="outlined" value={name} onChange={(e) => { setName(e.target.value);}}/>
+                    <br/><br/>
+                    <button className="join-btn">Join</button>
+                </div >
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default Join;
