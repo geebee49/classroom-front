67dcc44ea5d8da677af02a914e99becf81b96dd1
Maria Viji George <mariavijigeorge@gmail.com>
2021-09-01 15:30:07 +0530

    ask.jsx updated

--- src/components/ask.jsx
+++ src/components/ask.jsx
@@ -14,6 +14,37 @@ export default function LeaveClassroom() {
       setOpen(false);
     };
 
+    const [qnValue,setqnValue] = React.useState({input : '' });
+
+    const handleChangeQn =(e) => {
+      setqnValue({input : e.target.value});
+    }
+
+    const [opt1Value,setopt1Value] = React.useState({input : '' });
+
+    const handleChangeOpt1 =(e) => {
+      setopt1Value({input : e.target.value});
+    }
+
+    const [opt2Value,setopt2Value] = React.useState({input : '' });
+
+    const handleChangeOpt2 =(e) => {
+      setopt2Value({input : e.target.value});
+    }
+
+    const [opt3Value,setopt3Value] = React.useState({input : '' });
+
+    const handleChangeOpt3 =(e) => {
+      setopt3Value({input : e.target.value});
+    }
+
+    const handleClickSubmit = () => {
+      console.log(qnValue.input);
+      console.log(opt1Value.input);
+      console.log(opt2Value.input);
+      console.log(opt3Value.input);
+    }
+
     return (
         <div>
           <button className="ask_btn" onClick={handleClickOpen}>
@@ -28,11 +59,14 @@ export default function LeaveClassroom() {
           <div className="ask_title">
             Question
           </div>
-          <input type="textarea" name="textValue" className="qn-input" />
-
-
+          <input type="textarea" name="question" className="qn-input" onChange={handleChangeQn}/>
+          <div className="opt-input-container">
+          <input type="textarea" name="opt1" placeholder="Option 1" className="opt-input" onChange={handleChangeOpt1} />
+          <input type="textarea" name="opt2" placeholder="Option 2" className="opt-input" onChange={handleChangeOpt2} />
+          <input type="textarea" name="opt3" placeholder="Option 3" className="opt-input" onChange={handleChangeOpt3} />
+          </div>
             <div > 
-            <button className="submit_btn" >
+            <button className="submit_btn" type="submit" onClick={handleClickSubmit}>
             Submit
             </button>
             </div>
